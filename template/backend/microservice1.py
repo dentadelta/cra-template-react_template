e8adeebde4dce8e3c7eaedd9d2373ca10335db10
delta <65842385+dentadelta@users.noreply.github.com>
2022-10-13 06:15:59 +1000

    Update microservice1.py

--- template/backend/microservice1.py
+++ template/backend/microservice1.py
@@ -1,15 +1,27 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from fastapi.responses import FileResponse
 
 router = APIRouter()
 
-@router.get("/helloworld")
+@router.get("/Appjs")
 async def helloworld():
     return_data = {
-        'data1': 'Hello World',
-        'data2': 'Love is the answer',
-        'data3': [1, 2, 3, 4, 5],
-        'data4': {'a': 1, 'b': 2, 'c': 3}
+        'header': 'Heavy Vehicle',
+        'page_description': 'This application allows you to extract data from the heavy vehicle database',
+        'page_detail1':'This app contains the following functionality:',
+        'page_detail2':'This app contains the following roads',
+    }
+
+    return_array_data = {
+            'page_bullet_point': ['Eat', 'Sleep', 'Code', 'Repeat']
+    }
+
+    return_dict_data = {
+         'page_dict': {'road1': 'Road 1 description',
+                       'road2': 'Road 2 description',
+                       'road 3': 'Road 3 description'},
+                       
+         'url': {'logoUrl': 'logo192.png'}
         }
-    return return_data
+       
+    return {'data': return_data, 'dataarray': return_array_data, 'datadict': return_dict_data}
